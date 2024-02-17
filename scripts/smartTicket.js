ce0b75ec3d96985a45f1431cdf940e3ad30d4523
Deathknight101 <120172437+Deathknight101@users.noreply.github.com>
2024-02-18 02:55:51 +0600

    added banner button functionality

--- scripts/smartTicket.js
+++ scripts/smartTicket.js
@@ -1,5 +1,24 @@
 function BuyTicket() {
     const navigate = document.getElementById('ticket-book');
-    navigate.scrollIntoView({behavior:'smooth'});
+    navigate.scrollIntoView({ behavior: 'smooth' });
     console.log('buttonclicked');
+}
+
+const buttons= document.querySelectorAll('.btn');
+buttons.forEach(function(button) {
+    button.addEventListener("click", function onClick() {
+        let index=0;
+      // do something when the button is clicked
+      console.log("You clicked a button");
+      button.style.backgroundColor = 'lightgreen';
+    });
+  });
+
+function clicked(){
+    console.log('clicked');
+}
+function setBackgroundColorById(elementId)
+{
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-green-400');
 }
