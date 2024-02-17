faaa73cf40fa60774db3c9879cd56631176278b9
Deathknight101 <120172437+Deathknight101@users.noreply.github.com>
2024-02-18 01:17:05 +0600

    scrollview

--- scripts/smartTicket.js
+++ scripts/smartTicket.js
@@ -1,3 +1,5 @@
-function BuyTicket(){
-    console.log('button clicked');
+function BuyTicket() {
+    const navigate = document.getElementById('ticket-book');
+    navigate.scrollIntoView({behavior:'smooth'});
+    console.log('buttonclicked');
 }
